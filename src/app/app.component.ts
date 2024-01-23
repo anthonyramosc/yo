bac580b330a77afbfdbac2f5df92a843f3e93072
anthonyramosc <aaramos@sudamericano.edu.ec>
2024-01-23 11:03:13 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MiddleComponent } from './middle/middle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,MiddleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
